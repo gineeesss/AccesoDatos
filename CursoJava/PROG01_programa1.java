be81b8e1f53c9ee9535553f5d1d514d4891b1fdf
dam2 <ggutierrezs01@iescastelar.com>
2024-09-17 09:41:04 +0200

    Commit s/auto

---
+++ CursoJava/PROG01_programa1.java
@@ -0,0 +1,10 @@
+public class PROG01_programa1{
+	public static void main (String[] args){
+		System.out.println("Módulo Profesional - PROGRAMACIÓN. UNIDAD DE TRABAJO 01");
+		System.out.println ("Introducción a la programación");
+		System.out.println ("José Ignacio a la programación");
+		System.out.println ("Badajoz-Badajoz");
+		System.out.println ("16 de Octubre de 2024");
+		System.out.println ("Programa 01");
+	}
+}
---
+++ CursoJava/RepasoJava/src/repaso/ejercicio0.java
@@ -0,0 +1,8 @@
+package repaso;
+
+class ejercicio0 {
+	public static void main(String[] args) {
+		System.out.println("Primer ejercicio de JAVA");
+		System.out.println ("Prueba de Eclipse");
+	}
+}
