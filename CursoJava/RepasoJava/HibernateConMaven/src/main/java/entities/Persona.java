c6945c19d5d38b5750b155afb9612631f9235003
gineeesss <ggutierrezs01@iescastelar.com>
2024-11-12 14:13:23 +0100

    Commit s/auto

---
+++ CursoJava/RepasoJava/HibernateConMaven/src/main/java/entities/Persona.java
@@ -0,0 +1,12 @@
+package entities;
+
+import java.util.Date;
+
+public class Persona {
+	private int id;
+	private String nombre;
+	private int edad;
+	private Date fechaNacimiento;
+	public Persona() {}
+	
+}
---
+++ CursoJava/RepasoJava/Tema3HibernateMaven/src/main/java/conexion/Main.java
@@ -0,0 +1,23 @@
+package conexion;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.internal.MetadataImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Iniciando configuración hibernate....");
+		final StandardServiceRegistry registro = new StandardServiceRegistryBuilder().configure().build();
+		final SessionFactory factory = new MetadataSources(registro).buildMetadata().buildSessionFactory();
+		
+		System.out.println("Conectando a la base de datos....");
+		final Session session = factory.openSession();
+		
+		System.out.println("Conectando correctamente a la base de datos....");
+	}
+
+}
